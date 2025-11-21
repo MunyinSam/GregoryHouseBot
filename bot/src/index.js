cf71fa8ea9fc2a877a9700111dfe8d3a76865376
MunyinSam <kelvinsam233@gmail.com>
2025-11-21 15:51:02 +0700

    refactor: Remove unused command and event files to streamline the codebase

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,22 +1,20 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js')
+const { Client, IntentsBitField } = require('discord.js')
 const dotenv = require('dotenv')
 
 dotenv.config()
 
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMembers,
     ],
 })
 
-client.commands = new Collection();
-
-// Load Handlers
-require('./handlers/commandHandler')(client);
-require('./handlers/eventHandler')(client);
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is online.`);
+})
 
 // Log in the bot
 client.login(process.env.DISCORD_TOKEN);
